4d51daea41f4f609a56c2985db070ccd54feeffb
pwang1092 <plwang@usc.edu>
2024-12-06 21:38:09 -0800

    email system working

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import {sendEmail} from './Form.js'
+
+import emailjs from '@emailjs/browser'; 
+
+import React, {useRef} from 'react';
+
+import Email from './Form.js'
 
 function App() {
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Email></Email>
       </header>
     </div>
   );

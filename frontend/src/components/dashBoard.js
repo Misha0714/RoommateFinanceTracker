b7207b4ad0f06b1791e81beeb929a7a156afbff3
Zaki Cole <zacole@usc.edu>
2024-12-07 16:42:19 -0800

    made dashboard edits

--- frontend/src/components/dashBoard.js
+++ frontend/src/components/dashBoard.js
@@ -40,17 +40,24 @@ function DashBoard(){
                 flexGrow: 1,
                 display: "flex",
                 flexDirection: "column",
-                width: "3fr"
             }}>
                 <Box sx={{
                     color: "#283c7d",
                     fontWeight: "bold",
                     fontSize: "1.5rem",
                     padding: "1rem 0rem",
+                    textAlign: "center"
                 }}>Transactions Dashboard</Box>
                 <BudgetSummary />
                 <ActivityLog />
-                <ModalForm />
+                <Box sx={{
+                    width: "100%",
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                }}>
+                    <ModalForm/>
+                </Box>
             </Box>
         </Box>
     );

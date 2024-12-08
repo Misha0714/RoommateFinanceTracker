efa2c6ad3eb78d02b078b715669bbb35ccd5ce44
Zaki Cole <zacole@usc.edu>
2024-12-07 16:27:23 -0800

    Added routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
 import LoginForm from './components/LoginForm';
 import Footer from './components/footer'
@@ -9,28 +10,26 @@ import RegistrationForm  from "./components/registrationForm"
 import ModalForm from "./components/addExpenseForm";
 import DuoLoginForm from "./components/DuoLoginForm";
 import DashBoard from "./components/dashBoard"
+import LandingPage from "./LandingPage/LandingPage"
+import ArticlePage from "./ArticlePage/ArticlePage"
 import { Duo } from "@mui/icons-material";
+
+import SigninPage from "./signin/signinPage";
+import SignupPage from "./signup/signupPage";
+
 function App() {
   return (
-    <div className="App">
-      <div>
-        <DashBoard />
-      </div>
-      <div> 
-       <NavBar/>
-      </div>
-      <div>
-        <RegistrationForm/>
-        <LoginForm/>
-      </div>
-        <BudgetSummary/>
-        <ActivityLog/>
-        <ModalForm/>
-      <div>
-    
-      </div>
-        <DuoLoginForm/>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<NavBar />}>
+          <Route index element={<LandingPage />} />
+          <Route path="signin" element={<SigninPage />} />
+          <Route path="signup" element={<SignupPage />} />
+          <Route path="dashboard" element={<DashBoard />} />
+          <Route path="blog" element={<ArticlePage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ frontend/src/signin/signinPage.js
@@ -0,0 +1,13 @@
+import React from "react"
+import LoginForm from '../components/LoginForm';
+import { Card, Typography, Box, Divider } from "@mui/material";
+
+function SigninPage() {
+    return (
+        <Box>
+            <LoginForm />
+        </Box>
+    );
+}
+
+export default SigninPage;

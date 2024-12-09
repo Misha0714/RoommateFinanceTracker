d651a632d06a12f53aef21890d023a87b5dae308
Leena Elmashat <lelmasha@usc.edu>
2024-12-08 18:51:01 -0800

    added user files

---
+++ backend/src/main/java/com/example/user/User.java
@@ -0,0 +1,60 @@
+package com.example.user;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.*;
+
+@Entity // This tells Hibernate to make a table out of this class
+//@Table(name = "User")
+public class User {
+	
+  @Id
+  @GeneratedValue(strategy=GenerationType.IDENTITY)
+  private Integer user_id;
+
+  //@Column(nullable = false)
+  private String username;
+
+  //@Column(nullable = false)
+  private String email;
+  
+  //@Column(nullable = false)
+  private String password;
+  
+  public User() {
+	  
+  }
+
+  public Integer getId() {
+    return user_id;
+  }
+  public void setId(Integer user_id) {
+	    this.user_id = user_id;
+	    }
+
+  public String getUsername() {
+    return username;
+  }
+
+  public void setUsername(String name) {
+    this.username = name;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+  
+  public String getPassword() {
+	    return password;
+}
+
+  public void setPassword(String password) {
+	  
+    this.password = password;
+  }
+}
